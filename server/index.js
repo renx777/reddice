c372de48d3232ff18417065209e4992a85102d35
renx777 <arenukaprasad@gmail.com>
2018-02-07 16:26:32 +0530

    added hot reloding and configured webpack

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
 import path from 'path';
 
+// setup webpack
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+import webpackHotMiddleware from 'webpack-hot-middleware'
+
 let app = express();
+
+const compiler=webpack(webpackConfig)
+
+// setting hot reload so that you dont have to reload page everytime code is changed
+app.use(webpackMiddleware(compiler,{
+    hot:true,
+    publicPath:webpackConfig.output.publicPath,
+    noInfo:true
+}))
+app.use(webpackHotMiddleware(compiler))
+
 
 app.get('/',(req,res) => {
     // send file as response
---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default{
+    devtool:'eval-source-map',
+    
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+       
+        path.join(__dirname,'/client/index.js')
+    ],
+    output:{
+        filename:'bundle.js',
+        path:'/',
+        publicPath:'/'
+    },
+
+    plugins:[
+       
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    
+    module:{
+        loaders:[
+            {
+                test: /\.js$/,
+               
+                include: path.join(__dirname,'client'),
+                
+                loaders:['babel-loader'],
+                
+                
+            }
+        ]
+    },
+    resolve:{
+        extensions:['.js']
+    }
+   
+    
+}
